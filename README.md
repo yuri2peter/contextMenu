306a77d45d6812adf9768e093cbbf20649b69d13
Yuri2 <824831811@qq.com>
2017-08-02 17:26:01 +0800

    sofa

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # contextMenu
 自由定制页面元素的右键菜单。A free  way to customize the context menu.
+![demo](http://ojp71nnay.bkt.clouddn.com/contextMenu-demo.png)
+## Prepare
+
+1. import jquery
+2. import contextMenu.css
+3. import contextMenu.js
+
+## Usage
+
+~~~js
+$('#div1').setContextMenu([
+     'menu1', //html code
+     'menu2',
+     '|', //delimiter
+     [
+         'click me', //title
+         function (e) {alert($(e.data).attr('id'))} // onclick
+     ],
+ ]);
+ 
+ $('body').setContextMenu(true); //disable default menu
+ $('body').setContextMenu(false); //restore default menu
+ 
+~~~
+
+## More Subjects.
+[Win10-UI](https://github.com/yuri2peter/win10-ui) |
+[GearPHP](https://github.com/yuri2peter/gear_php)
---
+++ src/contextMenu.js
@@ -0,0 +1,65 @@
+//class name:yuri2-context-menu
+$(function () {
+    var ContextMenu={
+        setContextMenu:function (jq_dom, menu) {
+            if(typeof (jq_dom)==='string'){
+                jq_dom=$(jq_dom);
+            }
+            jq_dom.unbind('contextmenu');
+            jq_dom.on('contextmenu', function(e) {
+                if(menu){
+                    ContextMenu._renderContextMenu(e.clientX,e.clientY,menu,this);
+                    e.preventDefault();
+                    e.stopPropagation();
+                }
+            });
+        },
+        _className:'yuri2-context-menu',
+        _renderContextMenu:function (x,y,menu,trigger) {
+            this._removeContextMenu();
+            if(menu===true){return;}
+            var dom = $("<div class='"+ContextMenu._className+"'><ul></ul></div>");
+            $('body').append(dom);
+            var ul=dom.find('ul');
+            for(var i=0;i<menu.length;i++){
+                var item=menu[i];
+                if(item==='|'){
+                    ul.append($('<hr/>'));
+                    continue;
+                }
+                if(typeof(item)==='string'){
+                    ul.append($('<li>'+item+'</li>'));
+                    continue;
+                }
+                if(typeof(item)==='object'){
+                    var sub=$('<li>'+item[0]+'</li>');
+                    ul.append(sub);
+                    sub.click(trigger,item[1]);
+                    continue;
+                }
+            }
+            //修正坐标
+            if(x+150>document.body.clientWidth){x-=150}
+            if(y+dom.height()>document.body.clientHeight && document.body.clientHeight>0){y-=dom.height()}
+            dom.css({
+                top:y,
+                left:x,
+            });
+        },
+        _removeContextMenu:function () {
+            $('.'+ContextMenu._className).remove();
+        },
+    };
+    $(document).click(function () {
+        ContextMenu._removeContextMenu();
+    });
+    $(document).on('contextmenu','.'+ContextMenu._className,function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+    });
+    $.fn.extend({
+        setContextMenu:function (menu){
+            ContextMenu.setContextMenu($(this),menu);
+        }
+    });
+});
