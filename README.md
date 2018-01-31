95e88a56ca3f7661ab6ffc42c61a3ab3cedf096d
Yuri2 <824831811@qq.com>
2018-01-31 17:03:22 +0800

    2.1

--- README.md
+++ README.md
@@ -1,32 +1,82 @@
 # contextMenu
-A free  way to customize the context menu.自由定制页面元素的右键菜单。
+自由定制页面元素的右键菜单。
+
 ![demo](http://ojp71nnay.bkt.clouddn.com/contextMenu-demo.png)
-## Prepare
 
-1. clone and star the repository
-2. import jquery
-3. import contextMenu.css
-4. import contextMenu.js
+> 是的同类的插件很多，作者造轮子是因为当初寻找右键菜单插件的时候，找了几个都有bug，要么经不起环境的考验，只能跑通demo。如果你也在寻找一款右键菜单插件，不如试试这一款，应该不会让你失望。
 
-## Usage
+## 原理
+该插件通过指定的参数，渲染出一个漂亮的右键菜单，并绑定菜单项点击事件。
+
+需要的参数有：
+
+1. 右键事件e。指定一个事件e，它可能是通过原生js、jq，甚至vue捕捉的；该插件将获取点击的位置，并阻止事件冒泡，屏蔽默认的右键菜单。
+2. 菜单描述数组menu。menu数组决定了你想渲染出什么样的菜单。
+
+## 特色
+
+* 侵入性小，这个插件几乎不会影响全局，除了ContextMenu全局变量。
+* 兼容性好，在各个屏幕尺寸和有无滚动条的场景都有不错的渲染效果。
+* 支持多层嵌套的DOM触发的事件，以最里层为准（详见demo）。
+* 支持二级菜单。
+
+## 准备
+
+1. 下载源码(并点赞)
+2. 引入 jquery
+3. 引入 contextMenu.css
+4. 引入 contextMenu.js
+
+## 典型用法
 
 ~~~js
-            $('div.demo').contextmenu(function (e) {
+            $('body').contextmenu(function (e) {
                 var menu=[
-                    'menu1', //html code
+                    'menu1', //合理的html或纯文字
                     'menu2',
-                    '|', //delimiter
+                    '|', //分隔符
                     [
                         'click me', //title
-                        function (e) {alert($(e.data).attr('id'))} // onclick
+                        function (dom) {alert('Hi')} // 点击菜单项的回调
                     ],
                 ];
-                ContextMenu.render(e,menu,this)
+                ContextMenu.render(e,menu,this); //开始渲染
             });
-            $('body').contextmenu(function (e) {
-                ContextMenu.render(e,true,this)
-            }); //disable default menu
 ~~~
 
-## More Subjects.
-[Yuri2](https://github.com/yuri2peter/)
+## API
+`ContextMenu.render(e,menu,objThis)`
+
+**e**:点击事件对象，如`$('body').contextmenu(function (e){})`。
+
+**menu**:
+
+menu为`false`代表恢复为系统默认的菜单；
+
+menu为`true`代表禁用系统默认菜单，但是不渲染自定义菜单；
+
+menu为数组表示渲染自定义右键菜单；
+~~~js
+var menu=[
+    '文字1', //纯文字或html将直接被渲染，做为一个提示性菜单项
+    '文字2',
+    '|', //简单的一个分隔符
+    ['功能1',function(param){alert("功能1点击")}], //这种格式说明这个菜单项可以被点击并产生回调
+    [
+        '子菜单',[
+            '文字3',
+            '文字4',
+            '|',
+            ['功能2',function(param){alert("功能2点击")}],
+        ]
+    ] //声明一个子菜单，子菜单内部的声明格式和父级一样
+]
+~~~
+
+**param**:菜单点击回调的第一个参数
+
+## 其他
+注意：为了获得正确的屏幕尺寸，添加了一个`html,body:{height:100%}`的样式，请确保该样式生效不被覆盖。
+
+## 更多项目
+[Yuri2'Projects](https://github.com/yuri2peter/)
--- src/contextMenu.js
+++ src/contextMenu.js
@@ -1,19 +1,14 @@
 /**
- * contextMenu v2.0
+ * contextMenu v2.1
  * @author Yuri2(yuri2peter@qq.com)
  * @link https://github.com/yuri2peter/contextMenu
  * Enjoy! (●'◡'●)
- */
-/**
  * 基于jq的右键菜单（动态绑定）
  * @author Yuri2
  */
-
 window.ContextMenu={
     _className:'yuri2-context-menu',
-    render:function (e, menu, trigger) {
-        // return;//TODO for test
-        var x=e.clientX,y=e.clientY;
+    _stopProp:function (e) {
         if (e.cancelable) {
             // 判断默认行为是否已经被禁用
             if (!e.defaultPrevented) {
@@ -21,31 +16,54 @@ window.ContextMenu={
             }
         }
         e.stopPropagation();
+    },
+    render:function (e, menu, trigger) {
+        // return;//TODO for test
+        var x=e.clientX,y=e.clientY;
+        this._stopProp(e);
         this._removeContextMenu();
         if(menu===true){return;}
         if(typeof menu === 'object' && menu.length===0){menu=[['...']]}
         var dom = $("<div class='"+ContextMenu._className+"'><ul></ul></div>");
         $('body').append(dom);
         var ul=dom.find('ul');
-        for(var i=0;i<menu.length;i++){
-            var item=menu[i];
+        menu.forEach(function (item) {
             if(item==='|'){
                 ul.append($('<hr/>'));
-                continue;
             }
-            if(typeof(item)==='string'){
-                ul.append($('<li>'+item+'</li>'));
-                continue;
+            else if(typeof(item)==='string'){
+                ul.append($('<li><div class="title">'+item+'</div></li>'));
             }
-            if(typeof(item)==='object'){
-                var sub=$('<li>'+item[0]+'</li>');
+            else if(typeof(item)==='object'){
+                var sub=$('<li><div class="title">'+item[0]+'</div></li>');
                 ul.append(sub);
-                sub.click(trigger,item[1]);
-                continue;
+                if(typeof(item[1])==='object'){
+                    var subMenu=$("<div class='"+ContextMenu._className+" sub'><ul></ul></div>");
+                    sub.addClass('sub');
+                    sub.append(subMenu);
+                    item[1].forEach(function (t) {
+                        if(t==='|'){
+                            subMenu.append($('<hr/>'));
+                        }
+                        else if(typeof(t)==='string'){
+                            subMenu.append($('<li><div class="title">'+t+'</div></li>'));
+                        }
+                        else if(typeof(item)==='object'){
+                            var subLi=$('<li><div class="title">'+t[0]+'</div></li>');
+                            subMenu.append(subLi);
+                            subLi.click(trigger,t[1]);
+                            subLi.click(function () {ContextMenu._removeContextMenu();});
+                        }
+                    })
+                }
+                else if(typeof(item[1])==='function'){
+                    sub.click(trigger,item[1]);
+                    sub.click(function () {ContextMenu._removeContextMenu();});
+                }
             }
-        }
+        });
         //修正坐标
-        if(x+150>document.body.clientWidth){x-=150}
+        if(x+150>document.body.clientWidth){x-=150;ul.addClass('left')}
         if(y+dom.height()>document.body.clientHeight && document.body.clientHeight>0){y-=dom.height()}
         dom.css({
             top:y,
@@ -57,6 +75,7 @@ window.ContextMenu={
     },
 };
 
-$(document).click(function () {
+$(document).click(function (e) {
+    if ($(e.target).hasClass(ContextMenu._className) || $(e.target).parents('.'+ContextMenu._className).length > 0) return;
     ContextMenu._removeContextMenu();
 });
